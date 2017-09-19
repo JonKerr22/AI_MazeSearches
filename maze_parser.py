507d00e0d4f07f6b0727ef166c4dc4ab98895c7d
JonKerr22 <jrkerr22@gmail.com>
2017-09-19 18:57:00 -0500

    created node class

--- maze_parser.py
+++ maze_parser.py
@@ -43,17 +43,21 @@ class State:
         return self.map[y][x]
     def isWall(self, x, y):
         return self.getCoord(x,y) == '%'
-    def getTransitions(self):
-        return getTransitions(self,self.location[0],self.location[1])
-    def getTransitions(self, x, y):
-    	moves = [add_tuples((x,y), move) for move in POSSIBLE_MOVES]
+    def getTransitions(self, *args):
+    	if len(args) == 2:
+    		moves = [add_tuples((x,y), move) for move in POSSIBLE_MOVES]
+        elif len(args) == 0:
+        	moves = [add_tuples(self.location, move) for move in POSSIBLE_MOVES]
+        
         return filter(lambda coord: not self.isWall(coord[0],coord[1]), moves)
+#this class will be used to construct a connected graph from map from text file       
 class Node:
 	def __init__(self, coordinates, state):
 		self.x = coordinates[0]
 		self.y = coordinates[1]
 		self.visited = False
 		self.neighbors = state.getTransitions(x, y)
+		self.isTarget = state.getCoord(x,y) == '.'
 
 m1 = State("mediumMaze.txt")
 print(m1)
